1ea06a461af7718f2c4d3c8a037fe0162db1a73e
cwrichardkim93 <cwrichardkim93@wr-130-64-218-249.medford.tufts.edu>
2014-02-02 10:24:58 -0500

    more animation stuff

---
+++ GetMeOutOfHere/GetMeOutOfHere/MKMessageTable.m
@@ -0,0 +1,31 @@
+//
+//  MKMessageTable.m
+//  GetMeOutOfHere
+//
+//  Created by Richard Kim on 2/2/14.
+//  Copyright (c) 2014 KimMoskowitz. All rights reserved.
+//
+
+#import "MKMessageTable.h"
+
+@implementation MKMessageTable
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
